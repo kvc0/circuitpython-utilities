0eaca886d17e8196c5320df8b2149889855f1bec
warriorofwire <3454741+WarriorOfWire@users.noreply.github.com>
2020-01-19 13:25:15 -0800

    Add rotarybutton
    Wraps an Adafruit rotary encoder with events
    * on_increment: when the encoder turns
    * on_click: when you click the rotary button
    * on_longhold_hold: when you press and hold the button longer than a
        click
    * on_longhold_release: when you release the button after holding it
        longer than a click

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# circuitpython-utilities
+# Quality of life utilities for CircuitPython
+
+## RotaryButton
+[Made with this in mind](https://www.adafruit.com/product/377)
+
+### Why
+Event-oriented programming is nice.
+Declaring what happens in response to named impulses helps provide nicely
+  structured software.  The program loop is not littered with conditions
+  and the intent and purpose of each statement is salient.
+
+### Example program
+This blinks an LED in response to any input on an Adafruit rotary encoder.
+
+```
+def _get_hardware():
+    encoder = rotaryio.IncrementalEncoder(board.A4, board.A5)
+    pin = digitalio.DigitalInOut(board.D5)
+    pin.direction = digitalio.Direction.INPUT
+    pin.pull = digitalio.Pull.UP
+    button = Debouncer(pin)
+
+    led = digitalio.DigitalInOut(D2)
+    led.direction = digitalio.Direction.OUTPUT
+
+    return button, encoder, led
+
+def _toggle(pin):
+    pin.value = not pin.value
+
+def run():
+    # ----------------------------------------------
+    # Set up hardware
+    button, encoder, led = _get_hardware()
+    toggle_led = lambda: _toggle(led)
+    def toggle_led_times(amount):
+        for i in range(abs(amount)):
+            toggle_led()
+
+    # ----------------------------------------------
+    # Configure events for the program.
+    rotarybutton = RotaryButton(
+        rotary, button,
+        on_increment:        [toggle_led_times],
+        on_click:            [toggle_led],
+        on_longhold_hold:    [toggle_led],
+        on_longhold_release: [toggle_led]
+    )
+
+    # ----------------------------------------------
+    # Run program forever waiting for input.
+    while True:
+        rotarybutton.loop()
+
+if __name__ == '__main__':
+    run()
+```
---
+++ src/rotarybutton.py
@@ -0,0 +1,71 @@
+import time
+
+# Default event list
+_do_nothing = tuple()
+
+
+class RotaryButton:
+    def __init__(self, rotary, button,
+                 on_increment: list = _do_nothing,
+                 on_click: list = _do_nothing,
+                 longhold_duration: float = 0.3,
+                 on_longhold_hold: list = _do_nothing,
+                 on_longhold_release: list = _do_nothing
+    ):
+        """
+        Wraps an Adafruit rotary encoder with events.  Call loop() on this when you want to invoke your callbacks if
+          the button has been pressed.
+        https://www.adafruit.com/product/377
+
+        :param rotary: a rotaryio.RotaryEncoder
+        :param button: a digital pin adafruit_debouncer.Debounce
+        :param on_click: void ()
+        :param longhold_duration: float seconds: Shorter than this is a click, longer than this is a "longhold"
+        :param on_longhold_hold: void ()
+        :param on_longhold_release: void ()
+        :param on_increment: void (int amount)
+        """
+        self._rotary = rotary
+        self._button = button
+        self.on_click = on_click
+        self._longhold_duration = longhold_duration
+        self.on_longhold_hold = on_longhold_hold
+        self.on_longhold_release = on_longhold_release
+        self.on_increment = on_increment
+        self._rotaryposition = rotary.position
+        self._current_press_start = None
+        self._longheld = False
+
+    def loop(self):
+        self._button.update()  # for debounce.  Inspect .rose and .fell for press/release events
+        self._update_rotary_position()
+        self._update_button()
+
+    def _update_button(self):
+        if self._current_press_start:
+            assert not self._button.fell, 'button is already down, it has to come up first doesn\'t it?'
+            pressed_time = time.monotonic() - self._current_press_start
+            if self._button.rose:
+                self._longheld = False  # whether or not we longheld, we're definitely not longholding now
+                self._current_press_start = None
+                if pressed_time < self._longhold_duration:
+                    for listener in self.on_click:
+                        listener()
+                else:
+                    for listener in self.on_longhold_release:
+                        listener()
+            elif not self._longheld and pressed_time >= self._longhold_duration:
+                self._longheld = True
+                for listener in self.on_longhold_hold:
+                    listener()
+        elif self._button.fell:
+            self._current_press_start = time.monotonic()
+
+    def _update_rotary_position(self):
+        if self._rotaryposition == self._rotary.position:
+            return
+        difference = self._rotaryposition - self._rotary.position
+        self._rotaryposition = self._rotary.position
+        print('Rotary activity: {}'.format(difference))
+        for listener in self.on_increment:
+            listener(difference)
